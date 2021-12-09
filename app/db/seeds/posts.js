ebf46d742451c9b2c40435198ee3795712b00c01
fhyfr <fhaya.firman@gmail.com>
2021-12-10 00:16:59 +0700

    update seeder with faker

--- app/db/seeds/posts.js
+++ app/db/seeds/posts.js
@@ -1,15 +1,20 @@
+const faker = require("faker");
+const generatePost = Array(20)
+  .fill(0)
+  .map((_, i) => {
+    return {
+      title: faker.name.title(),
+      body: faker.lorem.sentence(),
+      slug: faker.lorem.slug(),
+    };
+  });
+
 exports.seed = (knex) => {
   // Deletes ALL existing entries
   return knex("posts")
     .del()
     .then(() => {
       // Inserts seed entries
-      return knex("posts").insert([
-        { title: "judul 1", body: "isi body-1", slug: "judul-1" },
-        { title: "judul 2", body: "isi body-2", slug: "judul-2" },
-        { title: "judul 3", body: "isi body-3", slug: "judul-3" },
-        { title: "judul 4", body: "isi body-4", slug: "judul-4" },
-        { title: "judul 5", body: "isi body-5", slug: "judul-5" },
-      ]);
+      return knex("posts").insert(generatePost);
     });
 };
--- app/db/seeds/users.js
+++ app/db/seeds/users.js
@@ -1,40 +1,21 @@
+const faker = require("faker");
+const generateUser = Array(20)
+  .fill(0)
+  .map((_, i) => {
+    return {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      address: faker.address.streetAddress(),
+      phone: faker.phone.phoneNumber(),
+    };
+  });
+
 exports.seed = (knex) => {
   // Deletes ALL existing entries
   return knex("users")
     .del()
     .then(() => {
       // Inserts seed entries
-      return knex("users").insert([
-        {
-          name: "nama user 1",
-          email: "user1@gmail.com",
-          address: "alamat 1",
-          phone: "081",
-        },
-        {
-          name: "nama user 2",
-          email: "user2@gmail.com",
-          address: "alamat 2",
-          phone: "082",
-        },
-        {
-          name: "nama user 3",
-          email: "user3@gmail.com",
-          address: "alamat 3",
-          phone: "083",
-        },
-        {
-          name: "nama user 4",
-          email: "user4@gmail.com",
-          address: "alamat 4",
-          phone: "084",
-        },
-        {
-          name: "nama user 5",
-          email: "user5@gmail.com",
-          address: "alamat 5",
-          phone: "085",
-        },
-      ]);
+      return knex("users").insert(generateUser);
     });
 };
